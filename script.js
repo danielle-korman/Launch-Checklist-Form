318098f75a47e61a2825a7348991d00eedaba061
Danielle Korman <dk@Danielles-MBP.attlocal.net>
2020-08-03 15:18:20 -0500

    final commit

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
-// Write your JavaScript code here!
+/* This block of code shows how to format the HTML once you fetch some planetary JSON!
 
+*/
 window.addEventListener("load", function() {
+   
+   fetch("https://handlers.education.launchcode.org/static/planets.json").then(function(response) {
+   response.json().then( function(json) {
+     const target = document.getElementById("missionTarget")
+     
+target.innerHTML = 
+`<h2>Mission Destination</h2>
+            <ol> 
+               <li>Name: ${json[3].name}</li>
+               <li>Diameter: ${json[3].diameter}</li>
+               <li>Star: ${json[3].star}</li>
+               <li>Distance from Earth: ${json[3].distance}</li>
+               <li>Number of Moons: ${json[3].moons}</li>
+            </ol>
+            <img src="${json[3].image}">
+            `;
+   });
+   } );
+
    let form = document.getElementById("launchForm");
    let button = document.getElementById("formSubmit");
 
@@ -42,23 +62,18 @@ window.addEventListener("load", function() {
          launchStatus.style.color = "red";
 
       }
+
       if (Number(cargoKg.value) > 10000) {
          faultyItems.style.visibility = "visible"; 
          cargoStatus.innerHTML = "Too much mass for launch";
          launchStatus.innerHTML = "Shuttle not ready for launch";
          launchStatus.style.color = "red";
       }
+
+      else {
+         launchStatus.style.color = "green";
+         launchStatus.innerHTML = "Shuttle is ready for launch";
+      }
       }
    });
 });
-/* This block of code shows how to format the HTML once you fetch some planetary JSON!
-<h2>Mission Destination</h2>
-<ol>
-   <li>Name: ${}</li>
-   <li>Diameter: ${}</li>
-   <li>Star: ${}</li>
-   <li>Distance from Earth: ${}</li>
-   <li>Number of Moons: ${}</li>
-</ol>
-<img src="${}">
-*/
